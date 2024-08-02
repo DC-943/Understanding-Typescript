3d42998be2c083b7c30c7beed5ac287c50660268
michael_chen943 <michael_chen943@aliyun.com>
2024-08-02 20:33:59 +0800

    core types number object

--- Section-2 TypeScript Basics- Basic Types/app.ts
+++ Section-2 TypeScript Basics- Basic Types/app.ts
@@ -1,11 +1,15 @@
-//console.log("Time to get started...!")
-function add(n1: number, n2: number) {
-  // if (typeof n1 !== "number" || typeof n2 !== "number") {
-  //   throw new Error("Incorrect input")
-  // }
-  return n1 + n2
+// const person: {
+//   name: string
+//   age: number
+// } =
+
+const person = {
+  name: "MAX",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
 }
-const number1 = 5
-const number2 = 2.8
-const result = add(number1, number2)
-console.log(result)
+
+let favoriteActivities: string[]
+favoriteActivities = ["Sports"]
+
+console.log(person.name)
