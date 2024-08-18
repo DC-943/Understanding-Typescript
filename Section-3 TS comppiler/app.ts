4242b52cdea401fa46310a4f50aa9afd015ce99d
michael_chen943 <michael_chen943@aliyun.com>
2024-08-18 23:45:22 +0800

    finish chapter-3

--- Section-3 TS comppiler/app.ts
+++ Section-3 TS comppiler/app.ts
@@ -1,1 +1,15 @@
-console.log('Time to get started...');
+const button = document.querySelector("button")!
+
+function add(n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2
+  }
+}
+
+function clickHandler(message: string) {
+  console.log("clicked!" + message)
+}
+
+if (button) {
+  button.addEventListener("click", clickHandler.bind(null, "Hello World!"))
+}
